46d5c93a09286441ad6cecf28bcaf656df472766
soft25 <sofiane.kirati25@gmail.com>
2019-06-12 15:25:22 +0200

    Better truffle.js

--- truffle.js
+++ truffle.js
@@ -2,7 +2,7 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   networks: {
-    ganache: {
+    my_local: {
       host: "localhost",
       port: 8546,
       network_id: "*"
